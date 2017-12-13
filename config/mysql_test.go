b2f0e1558a98b47e7cf883452e9c7dfaf2a1411a
Your Name <you@example.com>
2017-12-13 20:30:01 +0800

    fix

--- config/mysql_test.go
+++ config/mysql_test.go
@@ -1,36 +1,36 @@
 package config
 
-// import (
-// 	"io/ioutil"
-// 	"log"
-// 	"testing"
+import (
+	"io/ioutil"
+	"log"
+	"testing"
 
-// 	yaml "gopkg.in/yaml.v2"
-// )
+	yaml "gopkg.in/yaml.v2"
+)
 
-// func TestMysqlConnect(t *testing.T) {
-// 	var result int
-// 	data, err := ioutil.ReadFile("database.yml")
-// 	if err != nil {
-// 		panic(err)
-// 	}
+func TestMysqlConnect(t *testing.T) {
+	var result int
+	data, err := ioutil.ReadFile("database.yml")
+	if err != nil {
+		panic(err)
+	}
 
-// 	c := &MysqlConfig{}
-// 	if err = yaml.Unmarshal(data, c); err != nil {
-// 		panic(err)
-// 	}
+	c := &MysqlConfig{}
+	if err = yaml.Unmarshal(data, c); err != nil {
+		panic(err)
+	}
 
-// 	c.Env = "dev"
-// 	log.Printf("%v \n", c.Current())
+	c.Env = "dev"
+	log.Printf("%v \n", c.Current())
 
-// 	m, err := NewMysql(c)
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	m, err := NewMysql(c)
+	if err != nil {
+		panic(err)
+	}
 
-// 	sqlStr := "SELECT 1"
-// 	if err = m.Session.QueryRow(sqlStr).Scan(&result); err != nil {
-// 		panic(err)
-// 	}
-// 	log.Println(result)
-// }
+	sqlStr := "SELECT 1"
+	if err = m.Session.QueryRow(sqlStr).Scan(&result); err != nil {
+		panic(err)
+	}
+	log.Println(result)
+}
