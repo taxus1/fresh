263be310110abd1ea512ca4d6593e4da196a3767
taxus <taxus@admindeMacBook-Pro.local>
2018-01-09 14:27:13 +0800

    fix

--- model/ad_test.go
+++ model/ad_test.go
@@ -1,10 +1,15 @@
 package model
 
-// func TestLoadAd(t *testing.T) {
-// 	ads, err := LoadAds(9)
-// 	checkErr(err)
-//
-// 	for _, v := range *ads {
-// 		log.Printf("获取广告: %v \n", v)
-// 	}
-// }
+import (
+	"log"
+	"testing"
+)
+
+func TestLoadAd(t *testing.T) {
+	ads, err := LoadAds(9)
+	checkErr(err)
+
+	for _, v := range *ads {
+		log.Printf("获取广告: %v \n", v)
+	}
+}
